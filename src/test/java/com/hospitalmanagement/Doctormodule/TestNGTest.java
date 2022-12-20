acfce113acb64c89809c69a9e1b65af15050460a
Bhimraodadannavar <vickydadannavar@gmail.com>
2022-12-20 20:09:29 +0530

    Parameterization

---
+++ src/test/java/com/hospitalmanagement/Doctormodule/TestNGTest.java
@@ -0,0 +1,19 @@
+package com.hospitalmanagement.Doctormodule;
+
+import org.testng.annotations.Test;
+
+public class TestNGTest 
+{
+	@Test
+	public void parameterization()
+	{
+		String url = System.getProperty("url");
+		String username = System.getProperty("username");
+		String password = System.getProperty("password");
+		
+		System.out.println(url);
+		System.out.println(username);
+		System.out.println(password);
+	}
+
+}
