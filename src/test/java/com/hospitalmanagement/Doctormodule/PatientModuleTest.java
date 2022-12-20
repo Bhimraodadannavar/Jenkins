37fd54198fb8119fe646ab9fa1882c8fd964992d
Bhimraodadannavar <vickydadannavar@gmail.com>
2022-12-20 15:26:24 +0530

    Jenkins Second commit

---
+++ src/test/java/com/hospitalmanagement/Doctormodule/PatientModuleTest.java
@@ -0,0 +1,13 @@
+package com.hospitalmanagement.Doctormodule;
+
+import org.testng.annotations.Test;
+
+public class PatientModuleTest 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("Hi");
+	}
+
+}
